eb07096b5639995c7f1be6458aa78d17f1309662
Linyiqiang <lyq@buduobushao.com>
2019-04-07 18:33:22 +0800

    子修改

--- my.java
+++ my.java
@@ -1,18 +1,1 @@
-package com.example.allDemo;
-
-import android.app.Application;
-
-import com.squareup.leakcanary.LeakCanary;
-
-public class App extends Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        /*if (LeakCanary.isInAnalyzerProcess(this)) {//1
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }*/
-        LeakCanary.install(this);
-    }
-}
+子修改
